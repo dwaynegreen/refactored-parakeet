b887bf439b758886f01e879dd24ffc3f90818132
Dwayne Green <dgreen@1800contacts.com>
2018-03-02 09:26:46 -0700

    Initial Checkin - copy of starter project

---
+++ TestNinja.UnitTests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        [TestCase(1, 2, 3)]
+        public void Add_WhenCalled_ReturnsTheSumOfArguments(int a, int b, int result)
+        {
+            var math = new Fundamentals.Math();
+            var sum = math.Add(a, b);
+            Assert.That(sum, Is.EqualTo(result));
+        }
+    }
+}

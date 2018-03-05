dd7c4e78c8d602e20bf6b5092469d92402967b54
Dwayne Green <dgreen@1800contacts.com>
2018-03-05 16:37:06 -0700

    First Set of Tests Written for Math class Setup and Teardown included

--- TestNinja.UnitTests/Tests.cs
+++ TestNinja.UnitTests/Tests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Security.Cryptography;
 using NUnit.Framework;
 using TestNinja.Fundamentals;
 
@@ -7,13 +7,46 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
-        [Test]
+        private Math _math;   
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _math = null;
+        }
+
         [TestCase(1, 2, 3)]
         public void Add_WhenCalled_ReturnsTheSumOfArguments(int a, int b, int result)
         {
-            var math = new Fundamentals.Math();
-            var sum = math.Add(a, b);
+            var sum = _math.Add(a, b);
             Assert.That(sum, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var result = _math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var result = _math.Max(1, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_BothArgumentsEqual_ReturnsTheSameArguments()
+        {
+            var result = _math.Max(1, 1);
+            Assert.That(result, Is.EqualTo(1));
         }
     }
 }
